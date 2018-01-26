4c1d12b59ae9f45ebe8ab6025f859e26c3174890
rmarlowg <rmarlow@mail.greenriver.edu>
2018-01-26 14:41:26 -0500

    Added block header/Error Reporting

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <?php
+/* Ryan Marlow / Cynthia Pham
+   IT 328 Final Project - Green River Project Hub
+   index.php
+*/
+
 //Require the autoload file
+error_reporting(E_ALL);
+ini_set('display_errors',TRUE);
 require_once ('vendor/autoload.php');
 
 //Create an instance of the Base class
 $f3 = Base::instance();
+
+$f3->set('DEBUG',3);
 
 //Define a default route
 $f3->route('GET /', function() {
