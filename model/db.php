90e76da1985eac6f1a3fbf06f47f098e2629d04c
Cynthia Pham <cpham15@mail.greenriver.edu>
2018-03-12 20:50:24 -0400

    added functions to validate and insert links into database table

--- model/db.php
+++ model/db.php
@@ -1,7 +1,5 @@
 <?php
 
-    //require("/home/cphamgre/config.php");
-
     function connect()
     {
         try {
@@ -16,31 +14,43 @@
         }
     }
 
-    function addProject($title, $description, $client, $siteurl, $trello,
-                        $github, $login, $password, $status, $notes) {
+    function addProject($title, $description, $client, $login, $password, $status,
+                        $notes, $location, $companyurl, $contactname, $contactemail,
+                        $contactphone, $instructor, $class, $quarter, $years)
+    {
         global $dbh;
 
         //Define query
-        $sql = "INSERT INTO projects (title, description, client, siteurl, trello, github, login, 
-            password,status, notes) VALUES (:title,:description,:client,:siteurl,:trello,:github,:login,
-            :password,:status,:notes)";
+        $sql = "INSERT INTO projects (title, description, client, login, 
+                password,status, notes, location, companyurl, contactname, contactemail, contactphone, 
+                instructor, class, quarter, years) VALUES (:title,:description,:client,:login,
+                :password,:status,:notes, :location, :companyurl, :contactname, :contactemail, 
+                :contactphone, :instructor, :class, :quarter, :years)";
 
         //prepare the statement
         $statement = $dbh->prepare($sql);
 
-        $statement->bindParam(':title',$title, PDO::PARAM_STR);
-        $statement->bindParam(':description',$description, PDO::PARAM_STR);
-        $statement->bindParam(':client',$client, PDO::PARAM_STR);
-        $statement->bindParam(':siteurl',$siteurl, PDO::PARAM_STR);
-        $statement->bindParam(':trello',$trello, PDO::PARAM_STR);
-        $statement->bindParam(':github',$github, PDO::PARAM_STR);
-        $statement->bindParam(':login',$login, PDO::PARAM_STR);
-        $statement->bindParam(':password',$password, PDO::PARAM_STR);
-        $statement->bindParam(':status',$status,PDO::PARAM_STR);
-        $statement->bindParam(':notes',$notes, PDO::PARAM_STR);
+        $statement->bindParam(':title', $title, PDO::PARAM_STR);
+        $statement->bindParam(':description', $description, PDO::PARAM_STR);
+        $statement->bindParam(':client', $client, PDO::PARAM_STR);
+        $statement->bindParam(':login', $login, PDO::PARAM_STR);
+        $statement->bindParam(':password', $password, PDO::PARAM_STR);
+        $statement->bindParam(':status', $status, PDO::PARAM_STR);
+        $statement->bindParam(':notes', $notes, PDO::PARAM_STR);
+        $statement->bindParam(':location', $location, PDO::PARAM_STR);
+        $statement->bindParam(':companyurl', $companyurl, PDO::PARAM_STR);
+        $statement->bindParam(':contactname', $contactname, PDO::PARAM_STR);
+        $statement->bindParam(':contactemail', $contactemail, PDO::PARAM_STR);
+        $statement->bindParam(':contactphone', $contactphone, PDO::PARAM_STR);
+        $statement->bindParam(':instructor', $instructor, PDO::PARAM_STR);
+        $statement->bindParam(':class', $class, PDO::PARAM_STR);
+        $statement->bindParam(':quarter', $quarter, PDO::PARAM_STR);
+        $statement->bindParam(':years', $years, PDO::PARAM_STR);
 
         //Execute
         $statement->execute();
+        $id = $dbh->lastInsertId();
+        return $id;
 
     }
 
@@ -70,7 +80,11 @@
 
         global $dbh;
 
-        $sql = "SELECT * FROM projects";
+        $sql = "SELECT projects.*, GROUP_CONCAT(links.url SEPARATOR ', ') as url
+                    FROM projects
+                    LEFT JOIN ProjectLink ON ProjectLink.project_id = projects.project_id
+                    LEFT JOIN links ON ProjectLink.link_id = links.link_id
+                    GROUP BY projects.project_id";
 
         //2. Prepare the statement
         $statement = $dbh->prepare($sql);
@@ -82,7 +96,9 @@
         $result = $statement->fetchAll(PDO::FETCH_ASSOC);
 
         return $result;
+
     }
+
 
     function removeProject($title) {
         global $dbh;
@@ -134,9 +150,81 @@
         //Execute
         $statement->execute();
 
-}
+    }
 
-    //$row = $statement->fetch(PDO::FETCH_ASSOC);
-    //echo $row['name']. " the " . $row['color']." ".$row['type'];
+    function getNumOfLinks($url) {
+        global $dbh;
+
+        $sql = "SELECT * FROM links WHERE url = :url";
+
+        //2. Prepare the statement
+        $statement = $dbh->prepare($sql);
+
+        //3. Bind parameters
+        $statement->bindParam(':url', $url, PDO::PARAM_STR);
+
+        //4. Execute the query
+        $statement->execute();
+
+        $count = $statement->rowCount();
+
+        return $count;
+    }
+
+    function addLink($type, $url) {
+        global $dbh;
+
+        //Define query
+        $sql = "INSERT INTO links(type, url) VALUES (:type, :url)";
+
+        //prepare the statement
+        $statement = $dbh->prepare($sql);
+
+        $statement->bindParam(':type',$type, PDO::PARAM_STR);
+        $statement->bindParam(':url',$url, PDO::PARAM_STR);
+
+        //Execute
+        $statement->execute();
+        $id = $dbh->lastInsertId();
+        return $id;
+    }
+
+    function getLinkId($url) {
+        global $dbh;
+
+        $sql = "SELECT link_id FROM links WHERE url = :url";
+
+        //2. Prepare the statement
+        $statement = $dbh->prepare($sql);
+
+        //3. Bind parameters
+        $statement->bindParam(':url', $url, PDO::PARAM_STR);
+
+        //4. Execute the query
+        $statement->execute();
+
+        $link = $statement->fetch();
+
+        return $link['link_id'];
+    }
+
+    function addProjectLink($project_id, $link_id) {
+        global $dbh;
+
+        //Define query
+        $sql = "INSERT INTO ProjectLink(project_id, link_id) VALUES (:project_id, :link_id)";
+
+        //prepare the statement
+        $statement = $dbh->prepare($sql);
+
+        $statement->bindParam(':project_id',$project_id, PDO::PARAM_INT);
+        $statement->bindParam(':link_id',$link_id, PDO::PARAM_INT);
+
+        //Execute
+        $statement->execute();
+    }
+
+
+
 
 
---
+++ model/validation.php
@@ -0,0 +1,158 @@
+<?php
+
+    $linkArray = array("http://", "https://", "www.", "http://www.", "https://www.");
+    $trelloArray = array("http://trello.com/", "https://trello.com/", "www.trello.com/",
+        "http://www.trello.com/", "trello.com/", "https://www.trello/");
+    $githubArray = array("http://github.com/", "https://github.com/", "www.github.com/",
+        "http:www//github.com/", "github.com/", "https://www.github.com/");
+
+    /**
+     * @param $link
+     * @return mixed|string
+     */
+    function trimLink($link) {
+
+        global $linkArray;
+        $link = strtolower($link);
+        $link = trim($link);
+
+        foreach($linkArray as $val)
+        {
+            if (stripos($link, $val) !== FALSE)
+            {
+                $link = str_replace($val, "", $link);
+            }
+
+        }
+
+        $link = "https://www." . $link;
+        return $link;
+    }
+
+    /**
+     * @param $trello
+     * @return mixed|string
+     */
+    function trimTrello($trello) {
+
+        global $trelloArray;
+        $trello = strtolower($trello);
+        $trello = trim($trello);
+
+        foreach($trelloArray as $val)
+        {
+            if (stripos($trello, $val) !== FALSE)
+            {
+                $trello = str_replace($val, "", $trello);
+            }
+        }
+
+        $trello = "https://www.trello.com/" . $trello;
+        return $trello;
+    }
+
+    /**
+     * @param $github
+     * @return mixed|string
+     */
+    function trimGitHub($github) {
+
+        global $githubArray;
+        $github = strtolower($github);
+        $github = trim($github);
+
+        foreach($githubArray as $val)
+        {
+            if (stripos($github, $val) !== FALSE)
+            {
+                $github = str_replace($val, "", $github);
+            }
+        }
+
+        $github = "https://www.github.com/" . $github;
+        return $github;
+    }
+
+    //PHP Validation
+
+    /**
+     * @param $title
+     * @return string
+     */
+    function validateTitle($title) {
+        if($title != null)
+        {
+            return "Project title is required";
+        }
+    }
+
+    /**
+     * @param $client
+     * @return string
+     */
+    function validateClient($client)
+    {
+        if ($client != null) {
+            return "Client's name is required";
+        }
+        else if (!ctype_alpha($client))
+        {
+            return "Client's name needs to only be alphabetical.";
+        }
+    }
+
+    /**
+     * @param $description
+     * @return string
+     */
+    function validateDesc($description) {
+
+        if($description != null)
+        {
+            return "Project description is required.";
+        }
+        else if (strlen($description) >= 10)
+        {
+            return "Project description needs to be at least 10 characters long.";
+        }
+    }
+
+    //Javascript Regex Validation
+
+    /**
+     * Validates client's phone number
+     *
+     * Validates to see if phone number is 10 digits long.  Accepts the following format:
+     * xxxxxxxxxx, xxx-xxx-xxxx, (xxx)xxx-xxxx, (xxx)xxxxxxx.  If it doesn't match, it will
+     * print error message
+     * @param $contactPhone Client's phone number
+     * @return String error message that reads "invalid phone number format"
+     *
+     */
+    function validatePhone($contactPhone) {
+        $regexp = '/^\(?\d{3}\)?-?\d{3}-?\d{4}$/';
+
+        if (!preg_match($regexp, $contactPhone))
+        {
+            return "Invalid phone number format.";
+        }
+    }
+
+    /**
+     * Validates email address (case-insensitive)
+     *
+     * Validates to see if email starts with a letter and then any digits or letters repeated 0 or more times.
+     * Has @ character then any word group followed by . character between one or two times.  Must end in edu
+     * or com.  If it doesn't match, it will print error message
+     *
+     * @param $email Email Address
+     * @return String error message that reads "invalid email address"
+     */
+    function validateEmail($contactEmail) {
+        $regexp = '/^[a-z][a-z0-9]*@\w+\.(\w*.)?(com|edu)$/i';
+
+        if (!preg_match($regexp, $contactEmail))
+        {
+            return "Invalid email address.";
+        }
+    }
